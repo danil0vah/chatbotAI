9d31daa816012917f3b5314c4c56d85fcbd71560
Daniel <80539771+danil0vah@users.noreply.github.com>
2022-02-16 16:32:24 +0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -22,29 +22,29 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# Вернем мешок слов(подробнее см.Wiki):0 или 1 для каждого слова в мешке, которое существует в предложении
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+    # Токенизируем предложения/паттерны
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
+    # мешок слов предствален в виде матрицы из N слов
     bag = [0]*len(words)
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s:
-                # assign 1 if current word is in the vocabulary position
+                # присваиваем 1, если текущее слово находится в словарной позиции
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
+    # фильтруем предикты ниже порога  - ERROR_TRESHOLD
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    # сортируем по вероятности
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
